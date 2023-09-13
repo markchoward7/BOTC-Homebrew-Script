055186e55e2e82fe41ca415a538b892150ed6ed4
Mark Howard <mark.c.howard19@gmail.com>
2023-09-13 10:47:45 -0500

    enable vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import App from "App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+inject();
 root.render(<App />);
